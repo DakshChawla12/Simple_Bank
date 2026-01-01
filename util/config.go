91ff9be1b125eb4b4557e16ac7c497a1b5879fe0
Daksh Chawla <dakshf219@gmail.com>
2026-01-01 11:45:06 +0530

    changed log

--- util/config.go
+++ util/config.go
@@ -20,7 +20,7 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.AutomaticEnv()
 	err = viper.ReadInConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Println("config file not found, using environment variables")
 		return
 	}
 
